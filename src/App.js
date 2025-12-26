396fae7a224dc4d545c12448ba2842d4097264cf
Nick Gulson <nicholasgulson2021@u.northwestern.edu>
2025-12-26 14:17:11 -0600

    Github pages setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Grommet } from 'grommet';
 import { hpe } from 'grommet-theme-hpe';
 import QuoteList from './components/QuoteList';
